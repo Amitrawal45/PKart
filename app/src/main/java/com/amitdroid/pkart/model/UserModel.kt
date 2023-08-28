bff0be071d82278c5ff0aebd258c9ccea4d31735
Amit Rawal <atitrawal4505@gmail.com>
2023-08-28 15:57:34 +0530

    -init commit

---
+++ app/src/main/java/com/amitdroid/pkart/model/UserModel.kt
@@ -0,0 +1,10 @@
+package com.amitdroid.pkart.model
+
+data class UserModel(
+    val userName: String? ="",
+    val userCity: String? ="",
+    val userNumber: String? ="",
+    val userPinCode: String? ="",
+
+
+)
