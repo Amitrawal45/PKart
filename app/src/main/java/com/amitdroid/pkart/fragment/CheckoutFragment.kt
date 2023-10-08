93f9dadea956db4cd5108ea5ae6104eee49622ed
ghimi <ghimirekrishpan2345@gmail.com>
2023-10-08 17:36:47 +0530

    - initial commit

---
+++ app/src/main/java/com/amitdroid/pkart/fragment/CheckoutFragment.kt
@@ -0,0 +1,95 @@
+import android.content.Context
+import android.opengl.ETC1.isValid
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.amitdroid.pkart.R
+import com.amitdroid.pkart.adapter.CartAdapter
+import java.nio.Buffer
+
+class CheckoutFragment : Fragment() {
+
+    private val cartItems: List<CartItem> = listOf(
+        CartItem("Item 1", 10.0),
+        CartItem("Item 2", 15.0),
+    )
+
+    class CartItem {
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val rootView = inflater.inflate(R.layout.fragment_checkout, container, false)
+
+        // 2. Retrieve cart data and populate the UI with cart items
+        val cartItems = retrieveCartData()
+        val recyclerView = rootView.findViewById<RecyclerView>(R.id.cartRecyclerView)
+        val adapter = CartAdapter(cartItems as Context)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        // 3. Calculate and display the total price
+        val totalPriceTextView = rootView.findViewById<TextView>(R.id.totalPriceTextView)
+        val totalPrice = calculateTotalPrice(cartItems)
+        totalPriceTextView.text = "Total: $totalPrice USD"
+
+        // 6. Implement the checkout logic
+        val placeOrderButton = rootView.findViewById<Button>(R.id.placeOrderButton)
+        placeOrderButton.setOnClickListener {
+            // Gather billing and shipping information
+            val billingInfo = gatherBillingInfo()
+            val shippingInfo = gatherShippingInfo()
+
+            // Validate information and process the order
+            if (isValid(billingInfo as Buffer?) && isValid(shippingInfo as Buffer?)) {
+                processOrder(cartItems, billingInfo, shippingInfo)
+            } else {
+                // Display an error message if information is invalid
+                Toast.makeText(context, "Invalid information. Please check and try again.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        return rootView
+    }
+
+    private fun CartAdapter(context: Context): CartAdapter {
+
+
+
+
+
+
+    }
+
+    private fun processOrder(cartItems: Any, billingInfo: Any, shippingInfo: Any): Any {
+
+    }
+
+    private fun gatherShippingInfo(): Any {
+        TODO("Not yet implemented")
+    }
+
+    private fun gatherBillingInfo(): Any {
+        TODO("Not yet implemented")
+    }
+
+    private fun calculateTotalPrice(cartItems: Any): Any {
+        TODO("Not yet implemented")
+    }
+
+    private fun retrieveCartData(): Any {
+        TODO("Not yet implemented")
+    }
+
+    // Implement other functions like retrieveCartData, calculateTotalPrice,
+    // gatherBillingInfo, isValid, and processOrder based on your app's logic.
+}
